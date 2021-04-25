517aba6d964cbfa2748a703089ab2d7e3a4ecf82
Sverde-dot <sverdecadilla@danielcastelao.org>
2021-04-25 10:23:26 +0200

    Constraint Layout para la aplicacion + strings

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.ejemplo_constraint)
 
         val rollButton: Button = findViewById(R.id.roll)
         rollButton.setOnClickListener {
